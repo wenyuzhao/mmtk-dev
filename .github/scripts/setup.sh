6e78bc16974df47a85a62b4e6e76d7ba25160aa2
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-08-09 06:33:57 +0000

    Update setup.sh

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-sudo add-apt-repository ppa:openjdk-r/ppa -y
 sudo apt update && apt upgrade -y
 sudo apt install -y build-essential gdb libx11-dev libxext-dev libxrender-dev libxrandr-dev libxtst-dev libxt-dev libcups2-dev libasound2-dev default-jdk gcc-multilib libpfm4-dev gcc-multilib
 git submodule update --init --recursive
