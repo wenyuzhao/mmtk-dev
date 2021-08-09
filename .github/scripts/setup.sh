83fc974e8b85f0399f45643e9963b9947da65955
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-08-09 05:28:59 +0000

    Minor fix

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt update && apt upgrade
+sudo apt update && apt upgrade -y
 sudo apt install -y build-essential gdb libx11-dev libxext-dev libxrender-dev libxrandr-dev libxtst-dev libxt-dev libcups2-dev libasound2-dev
 git submodule update --init --recursive
 rustup toolchain add nightly-2021-05-12
