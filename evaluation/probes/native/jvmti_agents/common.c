0e4fbbae536725b391cde02eb1365d349b8d5c4c
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-10-22 10:56:30 +1100

    Delete old probes

--- evaluation/probes/native/jvmti_agents/common.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include "jvmti.h"
-
-void check_jvmti_error(jvmtiEnv *jvmti, jvmtiError err, char* msg) {
-    if (err != JVMTI_ERROR_NONE) {
-        printf("CHECK_JVMTI_ERROR: %s : %d\n", msg, err);
-        exit(err);
-    }
-}
