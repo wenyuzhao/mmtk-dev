179bef697c5914f53b28521fc190653056b8e8da
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-10-22 15:44:40 +1100

    cleanup

--- .github/scripts/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt update && apt upgrade -y
-sudo apt install -y build-essential gdb libx11-dev libxext-dev libxrender-dev libxrandr-dev libxtst-dev libxt-dev libcups2-dev libasound2-dev default-jdk gcc-multilib libpfm4-dev gcc-multilib
-git submodule update --init --recursive
-git lfs install
-git lfs pull
-rustup toolchain add nightly-2021-05-12
