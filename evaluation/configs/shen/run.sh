5d800ff1183483907f54b091a002704a92f030c2
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-11-19 12:21:58 +1100

    shen 5/10 x heap

---
+++ evaluation/configs/shen/run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -x
+
+hfac_args="10 9 16"
+
+config=$(basename $(dirname $0))
+
+pushd ~/MMTk-Dev
+
+pkill -f java -u wenyuz -9
+
+running runbms ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/$config/_config.yml $hfac_args --skip-oom 1 --skip-timeout 1 &> ~/_log
+
+popd
