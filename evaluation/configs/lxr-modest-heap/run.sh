6a1ab729d3cb6e30beebd7f8f8612f06483a4025
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-11-19 12:22:18 +1100

    update configs

--- evaluation/configs/lxr-modest-heap/run.sh
+++ evaluation/configs/lxr-modest-heap/run.sh
@@ -7,6 +7,8 @@ config=$(basename $(dirname $0))
 
 pushd ~/MMTk-Dev
 
+pkill -f java -u wenyuz -9
+
 running runbms ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/$config/_config.yml $hfac_args --skip-oom 1 --skip-timeout 1 &> ~/_log
 
 popd
