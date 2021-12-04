a04b4c28f3a85a8a1f3f4aedd0bb62d4cfa0122a
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-12-04 23:37:43 +1100

    Update

--- evaluation/configs/lxr-new-latency-curve/run.lu.sh
+++ evaluation/configs/lxr-new-latency-curve/run.lu.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -x
 
-hfac_args="8 4"
+# hfac_args="8 4"
+hfac_args="10 16"
 
 config=$(basename $(dirname $0))
 
@@ -9,7 +10,7 @@ pushd ~/MMTk-Dev
 
 pkill -f java -u wenyuz -9
 
-running runbms ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/$config/_config.lu.yml 8 4 --skip-oom 1 --skip-timeout 1 &> ~/_log
-# running runbms ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/$config/_config.2.yml 10 9 16 --skip-oom 1 --skip-timeout 1 &> ~/_log
+# running runbms ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/$config/_config.lu.yml 8 4 --skip-oom 1 --skip-timeout 1 &> ~/_log
+running runbms ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/$config/_config.2.yml 10 16 --skip-oom 1 --skip-timeout 1 &> ~/_log
 
 popd
