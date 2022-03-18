29a08692ceeaa76ae7423d31c48b9ad064240f35
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2022-03-19 00:05:50 +1100

    pause time curve

--- evaluation/configs/lxr-shepherd-pauses/run.sh
+++ evaluation/configs/lxr-shepherd-pauses/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -x
 
-hfac_args="8 4"
+hfac_args="12 12"
 
 config=$(basename $(dirname $0))
 
@@ -9,7 +9,7 @@ pushd ~/MMTk-Dev
 
 pkill -f java -u wenyuz -9
 
-running runbms ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/$config/_config.yml 8 4 &> ~/_log
+running runbms ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/$config/_config.yml 12 12 &> ~/_log
 # running runbms ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/$config/_config.2.yml 10 9 16 --skip-oom 1 --skip-timeout 1 &> ~/_log
 
 popd
