c703d48018267de095805cf435fea0928ced14e0
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2022-03-29 12:49:10 +1100

    wip

--- evaluation/configs/lxr-rc-evac/build.sh
+++ evaluation/configs/lxr-rc-evac/build.sh
@@ -10,11 +10,14 @@ export PATH=$HOME/.cargo/bin:$PATH
 
 
 
-rake jdk:test gc=Immix heap=2287M noc1=1 bench=lusearch profile=release n=5 features=lxr_heap_health_guided_gc,lxr_rc_only
+rake jdk:test gc=Immix heap=3287M noc1=1 bench=lusearch profile=release n=5 features=lxr_heap_health_guided_gc,lxr_rc_only
 rake bench:cp name=$config/jdk-lxr-rc-$branch
 
-rake jdk:test gc=Immix heap=2287M noc1=1 bench=lusearch profile=release n=5 features=lxr,lxr_heap_health_guided_gc,mmtk/lxr_incremental_defrag SIMPLE_INCREMENTAL_DEFRAG=8
-rake bench:cp name=$config/jdk-lxr-rc-evac-$branch
+# rake jdk:test gc=Immix heap=2287M noc1=1 bench=lusearch profile=release n=5 features=lxr,lxr_heap_health_guided_gc,mmtk/lxr_incremental_defrag SIMPLE_INCREMENTAL_DEFRAG=8
+# rake bench:cp name=$config/jdk-lxr-rc-evac-$branch
+
+rake jdk:test gc=Immix heap=20G   noc1=1 bench=lusearch n=5 profile=release features=lxr,lxr_heap_health_guided_gc,mmtk/lxr_incremental_defrag,mmtk/lxr_region_4m SIMPLE_INCREMENTAL_DEFRAG=8 SIMPLE_INCREMENTAL_DEFRAG_MULTIPLIER=8 NURSERY_BLOCKS=8192
+rake bench:cp name=$config/jdk-lxr-rc-evac-4m-$branch
 
 
 # rake jdk:test gc=Immix heap=2287M noc1=1 bench=lusearch profile=release n=5 features=lxr_heap_health_guided_gc,lxr_rc_only,mmtk/lxr_enable_initial_alloc_limit
--- evaluation/configs/minheap/build.sh
+++ evaluation/configs/minheap/build.sh
@@ -9,9 +9,9 @@ source $(dirname $0)/../utils.sh
 
 # NURSERY_RATIO=1 build_one jdk-lxr-old-fixleak-$branch lxr
 
-OPPORTUNISTIC_EVAC=1 OPPORTUNISTIC_EVAC_THRESHOLD=50 TRACE_THRESHOLD=30 INCS_LIMIT=20000 build_one jdk-lxr-rc-$branch lxr_rc_only
+TRACE_THRESHOLD=30 INCS_LIMIT=20000 build_one jdk-lxr-rc-$branch lxr_rc_only
 
-# OPPORTUNISTIC_EVAC=1 OPPORTUNISTIC_EVAC_THRESHOLD=50 TRACE_THRESHOLD=30 INCS_LIMIT=20000 build_one jdk-lxr-fix-$branch lxr,lxr_heap_health_guided_gc
+TRACE_THRESHOLD=30 INCS_LIMIT=20000 build_one jdk-lxr-$branch lxr,lxr_heap_health_guided_gc
 
 # pushd ~/MMTk-Dev/mmtk-core
 # git checkout lxr
