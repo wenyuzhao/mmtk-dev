02b36e57974f351b4dfdda06c148500ce5604a9f
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2022-04-10 15:23:49 +1000

    wip

--- evaluation/configs/lxr-final/run-plan-boar.sh
+++ evaluation/configs/lxr-final/run-plan-boar.sh
@@ -14,10 +14,10 @@ running runbms ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/lxr-final/
 # Shen 10x
 running runbms ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/lxr-final/config.shen10x.yml 12 12 &>> ~/_log
 # 1.3x latency dump
-running runbms ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/lxr-final-latency/config.yml 32 7 &>> ~/_log
+# running runbms ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/lxr-final-latency/config.yml 32 7 &>> ~/_log
 # 6x throughput
-running -i 5 runbms ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/lxr-final/config.p3-6.yml 12 12 &>> ~/_log
+# running -i 5 runbms ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/lxr-final/config.p3-6.yml 12 12 &>> ~/_log
 # Unfinished 1.3x
-running runbms ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/lxr-final/config.p3-boar-unfinished.yml 32 7 &>> ~/_log
+# running runbms ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/lxr-final/config.p3-boar-unfinished.yml 32 7 &>> ~/_log
 
 popd
