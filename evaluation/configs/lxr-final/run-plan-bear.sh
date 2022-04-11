b05dcb4fb68c2753909f3b038b214e6a49179a19
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2022-04-11 15:02:15 +1000

    wip

--- evaluation/configs/lxr-final/run-plan-bear.sh
+++ evaluation/configs/lxr-final/run-plan-bear.sh
@@ -12,12 +12,15 @@ pushd ~/MMTk-Dev
 # 1.3x latency dump
 # running runbms ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/lxr-final-latency/config.yml 32 7 &> ~/_log
 # 6x throughput
+
+running runbms -i 10 ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/lxr-final/config.p3.yml 12 7 &>> ~/_log
 running runbms -i 5 ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/lxr-final/config.p3-6.yml 12 12 &> ~/_log
+
 # Unfinished 1.3x
-running runbms ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/lxr-final/config.p3-bear-unfinished.yml 32 7 &>> ~/_log
+# running runbms ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/lxr-final/config.p3-bear-unfinished.yml 32 7 &>> ~/_log
 # Pauses
-running runbms -i 5 ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/lxr-final-pause/_config.yml 32 7 &>> ~/_log
-running runbms -i 5 ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/lxr-final-pause/config.punch.yml 12 7 &>> ~/_log
-running runbms -i 5 ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/lxr-final-pause/config.shen10x.yml 32 12 &>> ~/_log
+# running runbms -i 5 ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/lxr-final-pause/_config.yml 32 7 &>> ~/_log
+# running runbms -i 5 ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/lxr-final-pause/config.punch.yml 12 7 &>> ~/_log
+# running runbms -i 5 ./evaluation/results/log ~/MMTk-Dev/evaluation/configs/lxr-final-pause/config.shen10x.yml 32 12 &>> ~/_log
 
 popd
