12bfb8f2340728b2bb0380f356a70fbac01f94b1
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2022-10-06 05:55:50 +0000

    update

--- run-jdk.py
+++ run-jdk.py
@@ -7,7 +7,7 @@ MMTK_CORE = f'{MMTK_DEV}/mmtk-core'
 MMTK_OPENJDK = f'{MMTK_DEV}/mmtk-openjdk'
 OPENJDK = f'{MMTK_DEV}/openjdk'
 PROBES = f'{MMTK_DEV}/evaluation/probes'
-DACAPO = f'/usr/share/benchmarks/dacapo/dacapo-evaluation-git-29a657f.jar'
+DACAPO = f'/usr/share/benchmarks/dacapo/dacapo-evaluation-git-f480064.jar'
 BENCH_BUILDS = f'{MMTK_DEV}/evaluation/builds'
 
 
@@ -32,13 +32,14 @@ def parse_args():
     optional.add_argument('--features', type=str, nargs='*', help="Cargo features")
     optional.add_argument('--gdb', action='store_true', default=False, help="Launch GDB")
     optional.add_argument('--clean', action='store_true', default=False, help="`make clean` before build")
+    optional.add_argument('--exploded', action='store_true', default=False, help="Build exploded image")
+    optional.add_argument('--kill', action='store_true', default=False, help=f"Kill all existing Java and {os.path.basename(__file__)} processes")
     optional.add_argument('--cp-bench', dest='build_id', type=str, nargs='?', help=f"Copy build to {BENCH_BUILDS}/jdk-mmtk-<BUILD_ID>-<commit>")
     optional.add_argument('--jdk-args', type=str, nargs='?', help="Extra OpenJDK command line arguments")
     optional.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS, help='Show this help message and exit')
     return parser.parse_args()
 
-
-def exec(cmd: str, cwd: str):
+def exec(cmd: str, cwd: str = MMTK_DEV):
     print(f'🔵 {cmd}')
     result = os.system(f'cd {cwd} && {cmd}')
     if result != 0: raise RuntimeError(f'❌ {cmd}')
@@ -52,19 +53,23 @@ def clean(profile: str):
     exec(f'make clean --no-print-directory CONF=linux-x86_64-normal-server-{profile} THIRD_PARTY_HEAP={MMTK_OPENJDK}/openjdk', cwd=OPENJDK)
 
 
-def build(profile: str, bundle: bool):
-    product_bundles = 'product-bundles' if bundle else 'images'
-    exec(f'make --no-print-directory CONF=linux-x86_64-normal-server-{profile} THIRD_PARTY_HEAP={MMTK_OPENJDK}/openjdk {product_bundles}', cwd=OPENJDK)
+def build(profile: str, exploded: bool, bundle: bool):
+    if exploded:
+        assert not bundle, "cannot bundle an exploded image"
+        target = ''
+    else:
+        target = 'product-bundles' if bundle else 'images'
+    exec(f'make --no-print-directory CONF=linux-x86_64-normal-server-{profile} THIRD_PARTY_HEAP={MMTK_OPENJDK}/openjdk {target}', cwd=OPENJDK)
 
 
-def run(profile: str, gc: str, bench: str, heap: str, iter: int, noc1: bool, noc2: bool, gdb: bool, threads: Optional[int], mu: Optional[int], jdk_args: Optional[str]):
+def run(profile: str, gc: str, bench: str, heap: str, iter: int, noc1: bool, noc2: bool, gdb: bool, exploded: bool, threads: Optional[int], mu: Optional[int], jdk_args: Optional[str]):
     # MMTk args
     mmtk_args = f'RUST_BACKTRACE=1 MMTK_PLAN={gc}'
     if threads is not None: mmtk_args += f' MMTK_THREADS={threads}'
     # Heap size
     heap_args = f'-XX:MetaspaceSize=1G -XX:-UseBiasedLocking -Xms{heap} -Xmx{heap} -XX:+UseThirdPartyHeap'
     # Probes args
-    probe_args = f'-Dprobes=RustMMTk -Djava.library.path={PROBES} -cp {PROBES}:{PROBES}/probes.jar:{DACAPO}'
+    probe_args = f'--add-exports java.base/jdk.internal.ref=ALL-UNNAMED -Dprobes=RustMMTk -Djava.library.path={PROBES} -cp {PROBES}:{PROBES}/probes.jar:{DACAPO}'
     # Compiler args
     compiler_args = ''
     if noc1 and noc2: compiler_args += '-Xint'
@@ -78,7 +83,8 @@ def run(profile: str, gc: str, bench: str, heap: str, iter: int, noc1: bool, noc
     # Extra
     extra_jdk_args = jdk_args if jdk_args is not None else ''
     # Run
-    exec(f'{mmtk_args} {gdb_wrapper} {OPENJDK}/build/linux-x86_64-normal-server-{profile}/jdk/bin/java {extra_jdk_args} {heap_args} {compiler_args} {probe_args} Harness -n {iter} -c probe.DacapoChopinCallback {bench} {bm_args}', cwd=MMTK_DEV)
+    java = f'{OPENJDK}/build/linux-x86_64-normal-server-{profile}/jdk/bin/java' if exploded else f'{OPENJDK}/build/linux-x86_64-normal-server-{profile}/images/jdk/bin/java'
+    exec(f'{mmtk_args} {gdb_wrapper} {java} {extra_jdk_args} {heap_args} {compiler_args} {probe_args} Harness -n {iter} -c probe.DacapoChopinCallback {bench} {bm_args}', cwd=MMTK_DEV)
 
 
 def bench_copy(profile: str, target: str):
@@ -99,11 +105,17 @@ def bench_copy(profile: str, target: str):
     exec(f'rm {BENCH_BUILDS}/jdk-mmtk-{target}-{commit}.tar.gz', cwd=MMTK_DEV)
 
 
+def kill():
+    user = os.getlogin()
+    os.system(f'pkill -f java -u {user} -9')
+
+
 args = parse_args()
 
+if args.kill: kill()
 if args.config: config(profile=args.profile)
 if args.clean: clean(profile=args.profile)
-if args.build: build(profile=args.profile, bundle=args.build_id is not None)
-run(profile=args.profile, gc=args.gc, bench=args.bench, heap=args.heap, iter=args.iter, noc1=args.no_c1, noc2=args.no_c2, gdb=args.gdb, threads=args.threads, mu=args.mu, jdk_args=args.jdk_args)
+if args.build: build(profile=args.profile, exploded=args.exploded, bundle=args.build_id is not None)
+run(profile=args.profile, gc=args.gc, bench=args.bench, heap=args.heap, iter=args.iter, noc1=args.no_c1, noc2=args.no_c2, gdb=args.gdb, exploded=args.exploded, threads=args.threads, mu=args.mu, jdk_args=args.jdk_args)
 if args.build_id is not None:
     bench_copy(profile=args.profile, target=args.build_id)
