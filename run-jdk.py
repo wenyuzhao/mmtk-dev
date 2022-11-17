6d862c78306b87b2a7e5fcfe20072da4f6fb8519
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2022-11-17 05:36:35 +0000

    wip

--- run-jdk.py
+++ run-jdk.py
@@ -11,8 +11,14 @@ PROBES = f'{MMTK_DEV}/evaluation/probes'
 DACAPO = f'/usr/share/benchmarks/dacapo/dacapo-evaluation-git-6e411f33.jar'
 BENCH_BUILDS = f'{MMTK_DEV}/evaluation/builds'
 
+# HOTSPOT_GCS = ['G1', 'Shen', 'Shenandoah', 'Z', 'ZGC', 'Parallel', 'Serial']
+HOTSPOT_GCS = {
+    'G1': '-XX:+UseG1GC',
+}
 
 NO_BIASED_LOCKING = True
+NO_CLASS_UNLOAD = True
+XCOMP = False
 
 def parse_args():
     parser = argparse.ArgumentParser(description=f'MMTk-OpenJDK Runner.\n\nExample: ./{os.path.basename(__file__)} --gc=SemiSpace --bench=xalan --heap=100M --build', add_help=False, formatter_class=argparse.RawTextHelpFormatter)
@@ -33,7 +39,8 @@ def parse_args():
     optional.add_argument('--mu', type=int, nargs='?', help="Fix mutators")
     optional.add_argument('--threads', type=int, nargs='?', help="Fix GC workers")
     optional.add_argument('--features', type=str, nargs='*', help="Cargo features")
-    optional.add_argument('--gdb', action='store_true', default=False, help="Launch GDB")
+    optional.add_argument('--gdb', action='store_true', default=False, help="Launch with GDB")
+    optional.add_argument('--rr', action='store_true', default=False, help="Launch with rr record")
     optional.add_argument('--clean', action='store_true', default=False, help="`make clean` before build")
     optional.add_argument('--exploded', action='store_true', default=False, help="Build exploded image")
     optional.add_argument('--kill', action='store_true', default=False, help=f"Kill all existing Java and {os.path.basename(__file__)} processes")
@@ -48,7 +55,7 @@ def exec(cmd: str, cwd: str = MMTK_DEV):
     if result != 0: raise RuntimeError(f'❌ {cmd}')
 
 def config(profile: str):
-    exec(f'sh configure --disable-warnings-as-errors --with-debug-level={profile} --with-target-bits=64 --with-native-debug-symbols=internal --with-jvm-features=shenandoahgc', cwd=OPENJDK)
+    exec(f'sh configure --disable-warnings-as-errors --with-debug-level={profile} --with-target-bits=64 --with-jvm-features=shenandoahgc', cwd=OPENJDK)
     exec(f'make reconfigure CONF=linux-x86_64-normal-server-{profile}', cwd=OPENJDK)
 
 
@@ -67,13 +74,18 @@ def build(profile: str, exploded: bool, bundle: bool, features: Optional[str]):
     exec(f'make --no-print-directory CONF=linux-x86_64-normal-server-{profile} THIRD_PARTY_HEAP={MMTK_OPENJDK}/openjdk {target} {features}', cwd=OPENJDK)
 
 
-def run(profile: str, gc: str, bench: str, heap: Optional[str], iter: int, noc1: bool, noc2: bool, gdb: bool, exploded: bool, threads: Optional[int], mu: Optional[int], jdk_args: Optional[str]):
-    # MMTk args
-    mmtk_args = f'RUST_BACKTRACE=1 MMTK_PLAN={gc}'
-    if threads is not None: mmtk_args += f' MMTK_THREADS={threads}'
+def run(profile: str, gc: str, bench: str, heap: Optional[str], iter: int, noc1: bool, noc2: bool, gdb: bool, rr: bool, exploded: bool, threads: Optional[int], mu: Optional[int], jdk_args: Optional[str]):
+    # MMTk or HotSpot GC args
+    if gc in HOTSPOT_GCS:
+        hs_gc = HOTSPOT_GCS[gc] + f' -agentpath:{PROBES}/libperf_statistics.so'
+        mmtk_env_args = f'RUST_BACKTRACE=1 LD_PRELOAD={PROBES}/libperf_statistics.so'
+    else:
+        hs_gc = '-XX:+UseThirdPartyHeap'
+        mmtk_env_args = f'RUST_BACKTRACE=1 MMTK_PLAN={gc}'
+    if threads is not None: mmtk_env_args += f' MMTK_THREADS={threads}'
     # Heap size
     heap_size = f'-Xms{heap} -Xmx{heap}' if heap is not None else ''
-    heap_args = f'-XX:MetaspaceSize=1G {heap_size} -XX:+UseThirdPartyHeap'
+    heap_args = f'-XX:MetaspaceSize=1G {heap_size}'
     # Probes args
     probe_args = f'--add-exports java.base/jdk.internal.ref=ALL-UNNAMED -Dprobes=RustMMTk -Djava.library.path={PROBES} -cp {PROBES}:{PROBES}/probes.jar:{DACAPO}'
     # Compiler args
@@ -82,7 +94,12 @@ def run(profile: str, gc: str, bench: str, heap: Optional[str], iter: int, noc1:
     elif noc1: compiler_args += '-XX:-TieredCompilation'
     elif noc2: compiler_args += '-XX:TieredStopAtLevel=1'
     # GDB Wrapper
-    gdb_wrapper = 'gdb --args' if gdb else ''
+    if gdb:
+        debugger_wrapper = 'gdb --args'
+    elif rr:
+        debugger_wrapper = f'rr record'
+    else:
+        debugger_wrapper = ''
     # Benchmark args
     bm_args = f''
     if mu is not None: bm_args += f' -t {mu}'
@@ -90,9 +107,14 @@ def run(profile: str, gc: str, bench: str, heap: Optional[str], iter: int, noc1:
     extra_jdk_args = jdk_args if jdk_args is not None else ''
     if NO_BIASED_LOCKING:
         extra_jdk_args += ' -XX:-UseBiasedLocking'
+    if NO_CLASS_UNLOAD:
+        extra_jdk_args += ' -XX:-ClassUnloading -XX:-ClassUnloadingWithConcurrentMark'
+    if XCOMP:
+        extra_jdk_args += ' -Xcomp'
+    extra_jdk_args += ' -XX:+UnlockExperimentalVMOptions -XX:-UseCompressedOops -XX:-UseCompressedClassPointers'
     # Run
     java = f'{OPENJDK}/build/linux-x86_64-normal-server-{profile}/jdk/bin/java' if exploded else f'{OPENJDK}/build/linux-x86_64-normal-server-{profile}/images/jdk/bin/java'
-    exec(f'{mmtk_args} {gdb_wrapper} {java} {extra_jdk_args} {heap_args} {compiler_args} {probe_args} Harness -n {iter} -c probe.DacapoChopinCallback {bench} {bm_args}', cwd=MMTK_DEV)
+    exec(f'{mmtk_env_args} {debugger_wrapper} {java} {extra_jdk_args} {hs_gc} {heap_args} {compiler_args} {probe_args} Harness -n {iter} -c probe.DacapoChopinCallback {bench} {bm_args}', cwd=MMTK_DEV)
 
 
 def bench_copy(profile: str, target: str):
@@ -124,6 +146,6 @@ if args.kill: kill()
 if args.config: config(profile=args.profile)
 if args.clean: clean(profile=args.profile)
 if args.build: build(profile=args.profile, exploded=args.exploded, bundle=args.build_id is not None, features=args.features)
-run(profile=args.profile, gc=args.gc, bench=args.bench, heap=args.heap, iter=args.iter, noc1=args.no_c1, noc2=args.no_c2, gdb=args.gdb, exploded=args.exploded, threads=args.threads, mu=args.mu, jdk_args=args.jdk_args)
+run(profile=args.profile, gc=args.gc, bench=args.bench, heap=args.heap, iter=args.iter, noc1=args.no_c1, noc2=args.no_c2, gdb=args.gdb, rr=args.rr, exploded=args.exploded, threads=args.threads, mu=args.mu, jdk_args=args.jdk_args)
 if args.build_id is not None:
     bench_copy(profile=args.profile, target=args.build_id)
