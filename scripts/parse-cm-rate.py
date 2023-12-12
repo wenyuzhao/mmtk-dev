c7cd10e7342d7f0ef9b84df5651d831523250341
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2023-12-13 00:05:32 +1100

    update scripts

--- scripts/parse-cm-rate.py
+++ scripts/parse-cm-rate.py
@@ -1,14 +1,15 @@
 import re
+# import env
 
 
 def parse_log(file):
     record = False
-    cm_time = None
-    cm_objs = None
     cm_rate = []
     with open(file) as f:
         for line in f:
-            if "Early terminate SATB:" in line:
+            if "===== Chopin 23.11-chopin" in line and "PASSED" in line:
+                break
+            if "FinalMark start" in line:
                 record = True
                 continue
             if "finished." in line:
@@ -16,15 +17,15 @@ def parse_log(file):
                 continue
             if not record:
                 continue
-            match = re.search(r"STW_CM_PACKETS_TIME=(\d+)ms", line)
+            match = re.search(r"STW_CM_PACKETS_TIME=(\d+)ms .* STW_SCAN_NON_NULL_SLOTS=(\d+)", line)
             if match:
                 cm_time = float(match.groups()[0])
-            match = re.search(r"STW_SCAN_OBJS=(\d+)", line)
-            if match:
-                cm_objs = float(match.groups()[0])
-                assert cm_time is not None
-                cm_rate.append((cm_time, cm_objs, cm_objs / cm_time))
-    print(cm_rate)
+                cm_objs = float(match.groups()[1])
+                if cm_time > 0 and cm_objs > 0:
+                    cm_rate.append((cm_time, cm_objs, cm_objs / cm_time))
+    # print(cm_rate)
+    for v in cm_rate:
+        print(f"{v[2]:.1f}")
 
 
 parse_log("x.log")
