70f919682e5a19be94cccaecb814f17fd1db57ff
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2024-05-15 07:05:02 +0000

    wip

--- scripts/mmtk_dev/jdk/bench.py
+++ scripts/mmtk_dev/jdk/bench.py
@@ -2,12 +2,14 @@ import os
 from pathlib import Path
 import subprocess
 import sys
+from typing import Any
 import yaml
 from mmtk_dev.constants import MMTK_DEV, EVALUATION_DIR, OPENJDK, USERNAME
 from dataclasses import dataclass
 from simple_parsing import field
 from mmtk_dev.utils import ᐅᐳᐳ
-from .run import Run as RunJDK
+from .run import Run as RunJDK, Build as BuildJDK
+import re
 
 
 def _find_config_file(config: str):
@@ -42,6 +44,9 @@ class Build:
     clean: bool = field(default=False, negative_prefix="--no-")
     """Clean the build directory"""
 
+    reconfigure: bool = field(default=False, negative_prefix="--no-")
+    """reconfigure the build directory"""
+
     allow_dirty: bool = field(default=False, negative_prefix="--no-")
     """Allow dirty workspace"""
 
@@ -67,35 +72,62 @@ class Build:
         if os.system(f"cd {repo} && git checkout {commit}") != 0:
             sys.exit(f"❌ Failed to checkout {commit} for repo {repo.name}")
 
-    def __build_one(self, runtime_name: str, build_name: str, features: str | None, config: bool):
+    def __build_one(self, runtime_name: str, build_name: str, features: str | None, exploded: bool, test_command: str | None):
         try:
-            run = RunJDK(gc=self.gc, bench="fop", heap="500M", build=True, release=True, features=features, config=config or self.clean, clean=self.clean, bundle=True)
-            run.run()
-            self.__copy_jdk_bundle(build_name)
+            if test_command is None:
+                run = RunJDK(gc=self.gc, bench="fop", heap="500M", build=True, release=True, features=features, config=self.reconfigure or self.clean, clean=self.clean, bundle=not exploded, exploded=exploded)
+                run.run()
+            else:
+                build = BuildJDK(release=True, features=features, config=self.reconfigure or self.clean, clean=self.clean, bundle=not exploded, exploded=exploded)
+                build.run()
+                result = os.system(test_command)
+                assert result == 0, f"❌ Test command failed with exit code {result}"
+
+            if exploded:
+                self.__copy_jdk_bundle_exploded(build_name)
+            else:
+                self.__copy_jdk_bundle(build_name)
         except BaseException as e:
             sys.exit(f"❌ Failed to build `runtimes.{runtime_name}`!")
+
+    def __copy_jdk_bundle_exploded(self, target: str):
+        builds_dir = EVALUATION_DIR / "builds"
+        builds_dir.mkdir(parents=True, exist_ok=True)
+        # Delete previous builds
+        ᐅᐳᐳ("rm", "-rf", builds_dir / target)
+        ᐅᐳᐳ("rm", "-f", builds_dir / f"{target}.tar.gz")
+        # Copy exploded jdk folder
+        (builds_dir / target).mkdir(parents=True, exist_ok=True)
+        ᐅᐳᐳ("cp", "-r", OPENJDK / "build" / "linux-x86_64-normal-server-release" / "jdk", builds_dir / target / "jdk")
 
     def __copy_jdk_bundle(self, target: str):
         builds_dir = EVALUATION_DIR / "builds"
         builds_dir.mkdir(parents=True, exist_ok=True)
         # Get bundle file
-        bundle = (
-            subprocess.check_output(
-                ["bash", "-c", f"ls {OPENJDK}/build/linux-x86_64-normal-server-release/bundles/*.tar.gz | grep -v -e symbols -e demos"],
-                cwd=MMTK_DEV,
-            )
-            .decode("utf-8")
-            .strip()
-        )
+        bundle = subprocess.check_output(["bash", "-c", f"ls {OPENJDK}/build/linux-x86_64-normal-server-release/bundles/*.tar.gz | grep -v -e symbols -e demos"], cwd=MMTK_DEV).decode("utf-8").strip()
         # Delete previous builds
-        ᐅᐳᐳ("rm", "-rf", builds_dir / target, cwd=MMTK_DEV)
-        ᐅᐳᐳ("rm", "-f", builds_dir / f"{target}.tar.gz", cwd=MMTK_DEV)
+        ᐅᐳᐳ("rm", "-rf", builds_dir / target)
+        ᐅᐳᐳ("rm", "-f", builds_dir / f"{target}.tar.gz")
         # Copy bundle file
-        ᐅᐳᐳ("cp", bundle, builds_dir / f"{target}.tar.gz", cwd=MMTK_DEV)
+        ᐅᐳᐳ("cp", bundle, builds_dir / f"{target}.tar.gz")
         # Extract and remove bundle file
-        ᐅᐳᐳ("mkdir", "-p", builds_dir / target, cwd=MMTK_DEV)
-        ᐅᐳᐳ("tar", "-xf", builds_dir / f"{target}.tar.gz", "-C", builds_dir / target, cwd=MMTK_DEV)
-        ᐅᐳᐳ("rm", "-f", builds_dir / f"{target}.tar.gz", cwd=MMTK_DEV)
+        ᐅᐳᐳ("mkdir", "-p", builds_dir / target)
+        ᐅᐳᐳ("tar", "-xf", builds_dir / f"{target}.tar.gz", "-C", builds_dir / target)
+        ᐅᐳᐳ("rm", "-f", builds_dir / f"{target}.tar.gz")
+
+    def __check_builds(self, runtimes: Any):
+        builds: set[str] = set()
+        for runtime_name, runtime in runtimes.items():
+            home: str | None = runtime.get("home")
+            assert home is not None, f"❌ `runtimes.{runtime_name}.home` is not defined"
+            assert home not in builds, f"❌ Duplicated value for `runtimes.{runtime_name}.home`: {home}"
+            builds.add(home)
+            exploded: bool = runtime.get("exploded", False)
+            if exploded:
+                assert home.endswith("/jdk"), f"❌ Invalid value for `runtimes.{runtime_name}.home`: {home} (must end with /jdk for exploded builds)"
+            else:
+                jdk_folder_name = home.split("/")[-1]
+                assert re.match(r"jdk-\d+\.\d+\.\d+", jdk_folder_name), f"❌ Invalid value for `runtimes.{runtime_name}.home`: {home} (must end with /jdk-<version>)"
 
     def run(self):
         os.environ["BUILDS"] = f"{EVALUATION_DIR}/builds"
@@ -103,22 +135,24 @@ class Build:
         config_file = _find_config_file(self.config)
         with open(config_file, "r") as file:
             doc = yaml.safe_load(file)
-            for runtime_name in doc["runtimes"]:
-                commits = doc["runtimes"][runtime_name]["commits"]
-                features = doc["runtimes"][runtime_name].get("features")
+            self.__check_builds(doc["runtimes"])
+            for runtime_name, runtime in doc["runtimes"].items():
+                commits = runtime["commits"]
+                features = runtime.get("features")
+                exploded = runtime.get("exploded", False)
+                test_command = runtime.get("test-command")
                 # checkout commits
                 print(f"🟢 [{runtime_name}]: mmtk-core@{commits['mmtk-core']} mmtk-openjdk@{commits['mmtk-openjdk']} openjdk@{commits['openjdk']} features={features}")
                 self.__checkout(MMTK_DEV / "mmtk-core", commits["mmtk-core"])
                 self.__checkout(MMTK_DEV / "mmtk-openjdk", commits["mmtk-openjdk"])
                 self.__checkout(MMTK_DEV / "openjdk", commits["openjdk"])
-                reconfig_jdk = doc["runtimes"][runtime_name].get("reconfigure", False)
                 # build and copy target
-                features = doc["runtimes"][runtime_name].get("features")
-                home: str = os.path.expandvars(doc["runtimes"][runtime_name]["home"])
+                features = runtime.get("features")
+                home: str = os.path.expandvars(runtime["home"])
                 if home.endswith("/"):
                     home = home[:-1]
                 build_name = os.path.split(os.path.split(home)[0])[1]
-                self.__build_one(runtime_name=runtime_name, build_name=build_name, features=features, config=reconfig_jdk)
+                self.__build_one(runtime_name=runtime_name, build_name=build_name, features=features, exploded=exploded, test_command=test_command)
                 assert os.path.isfile(f"{home}/release"), f"❌ Failed to build `runtimes.{runtime_name}`: {home}/release does not exist"
                 print(f"✅ [{runtime_name}]: Build successful\n\n\n")
 
