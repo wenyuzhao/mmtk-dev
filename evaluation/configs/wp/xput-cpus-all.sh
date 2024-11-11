cac80c52ef0c269eca6d0968fc0e2ae5681953f7
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2024-11-11 01:23:10 +0000

    Update

--- evaluation/configs/wp/xput-cpus-all.sh
+++ evaluation/configs/wp/xput-cpus-all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# mmtk-jdk bench run --config wp/xput-cpus-a --invocations 5
+mmtk-jdk bench run --config wp/xput-cpus-a --invocations 5
 mmtk-jdk bench run --config wp/xput-cpus-b --invocations 5
 # mmtk-jdk bench run --config wp/xput-cpus-a --invocations 5
 # mmtk-jdk bench run --config wp/xput-cpus-b --invocations 5
